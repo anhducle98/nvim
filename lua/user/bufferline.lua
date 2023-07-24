4cdc14d47ea05e17b9c0234d8f403d73fdd8a294
Anh Duc Le <ale@dytechlab.com>
2023-07-24 15:56:59 +0800

    No italic bufferline

--- lua/user/bufferline.lua
+++ lua/user/bufferline.lua
@@ -10,7 +10,8 @@ local M = {
   },
 }
 function M.config()
-  require("bufferline").setup {
+  local bufferline = require("bufferline")
+  bufferline.setup {
     options = {
       mode = "buffers", -- set to "tabs" to only show tabpages instead
       themable = true, -- allows highlight groups to be overriden i.e. sets highlights as default
@@ -19,6 +20,7 @@ function M.config()
       right_mouse_command = "bdelete! %d", -- can be a string | function | false, see "Mouse actions"
       left_mouse_command = "buffer %d",    -- can be a string | function, | false see "Mouse actions"
       middle_mouse_command = nil,          -- can be a string | function, | false see "Mouse actions"
+      style_preset = bufferline.style_preset.no_italic,
       indicator = {
         icon = '▎', -- this should be omitted if indicator style is not 'icon'
         style = 'icon', -- | 'underline' | 'none',
