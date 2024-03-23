f39756376066a560a875dffbf97249c9482b7ee7
Anh-Duc Le <anhducle98@gmail.com>
2024-03-24 01:10:58 +0800

    Update

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,6 +1,5 @@
 local M = {
   "nvim-lualine/lualine.nvim",
-  -- commit = "0050b308552e45f7128f399886c86afefc3eb988",
   event = { "VimEnter", "InsertEnter", "BufReadPre", "BufAdd", "BufNew", "BufReadPost" },
 }
 
@@ -10,31 +9,6 @@ function M.config()
     return
   end
 
-  local hide_in_width = function()
-    return vim.fn.winwidth(0) > 80
-  end
-
-  local diagnostics = {
-    "diagnostics",
-    sources = { "nvim_diagnostic" },
-    sections = { "error", "warn" },
-    symbols = { error = " ", warn = " " },
-    colored = false,
-    always_visible = true,
-  }
-
-  local diff = {
-    "diff",
-    colored = false,
-    symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
-    cond = hide_in_width,
-  }
-
-  local filetype = {
-    "filetype",
-    icons_enabled = false,
-  }
-
   local location = {
     "location",
     padding = 0,
@@ -43,6 +17,7 @@ function M.config()
   local spaces = function()
     return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
   end
+
   lualine.setup {
     options = {
       globalstatus = true,
@@ -51,17 +26,19 @@ function M.config()
       theme = "onedark",
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
-      disabled_filetypes = { "alpha", "dashboard" },
+      disabled_filetypes = { "alpha", "dashboard", "Nvim-Tree" },
+      ignore_focus = { "Nvim-Tree" },
       always_divide_middle = true,
     },
     sections = {
       lualine_a = { "mode" },
       lualine_b = { "branch" },
-      lualine_c = { diagnostics },
-      lualine_x = { diff, spaces, "encoding", filetype },
+      lualine_c = {'filename'},
+      lualine_x = { spaces, "encoding", "filetype" },
       lualine_y = { location },
       lualine_z = { "progress" },
     },
+    extensions = { "nvim-tree" },
   }
 end
 
--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -1,82 +1,83 @@
 local M = {
   "kyazdani42/nvim-tree.lua",
-  commit = "59e65d88db177ad1e6a8cffaafd4738420ad20b6",
+  -- commit = "59e65d88db177ad1e6a8cffaafd4738420ad20b6",
   event = "VimEnter"
 }
 
 function M.config()
-  local tree_cb = require("nvim-tree.config").nvim_tree_callback
-  require("nvim-tree").setup {
-    disable_netrw = true,
-    hijack_netrw = true,
-    open_on_tab = false,
-    hijack_cursor = true,
-    hijack_unnamed_buffer_when_opening = false,
-    update_focused_file = {
-      enable = true,
-      update_root = false,
-    },
-    sync_root_with_cwd = false,
-    prefer_startup_root = true,
-    respect_buf_cwd = false,
-    renderer = {
-      icons = {
-        glyphs = {
-          default = "",
-          symlink = "",
-          folder = {
-            arrow_open = "",
-            arrow_closed = "",
-            default = "",
-            open = "",
-            empty = "",
-            empty_open = "",
-            symlink = "",
-            symlink_open = "",
-          },
-          git = {
-            unstaged = "",
-            staged = "S",
-            unmerged = "",
-            renamed = "➜",
-            untracked = "U",
-            deleted = "",
-            ignored = "◌",
-          },
-        },
-      },
-    },
-    diagnostics = {
-      enable = true,
-      show_on_dirs = true,
-      icons = {
-        hint = "",
-        info = "",
-        warning = "",
-        error = "",
-      },
-    },
-    view = {
-      width = 35,
-      side = "left",
-      mappings = {
-        list = {
-          { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-          { key = "h",                  cb = tree_cb "close_node" },
-          { key = "v",                  cb = tree_cb "vsplit" },
-        },
-      },
-    },
-    git = {
-      enable = true,
-      ignore = false,
-    },
-    actions = {
-      open_file = {
-        resize_window = true,
-      },
-    },
-  }
+  require("nvim-tree").setup {}
+  -- local tree_cb = require("nvim-tree.config").nvim_tree_callback
+  -- require("nvim-tree").setup {
+  --   disable_netrw = true,
+  --   hijack_netrw = true,
+  --   open_on_tab = false,
+  --   hijack_cursor = true,
+  --   hijack_unnamed_buffer_when_opening = false,
+  --   update_focused_file = {
+  --     enable = true,
+  --     update_root = false,
+  --   },
+  --   sync_root_with_cwd = false,
+  --   prefer_startup_root = true,
+  --   respect_buf_cwd = false,
+  --   renderer = {
+  --     icons = {
+  --       glyphs = {
+  --         default = "",
+  --         symlink = "",
+  --         folder = {
+  --           arrow_open = "",
+  --           arrow_closed = "",
+  --           default = "",
+  --           open = "",
+  --           empty = "",
+  --           empty_open = "",
+  --           symlink = "",
+  --           symlink_open = "",
+  --         },
+  --         git = {
+  --           unstaged = "",
+  --           staged = "S",
+  --           unmerged = "",
+  --           renamed = "➜",
+  --           untracked = "U",
+  --           deleted = "",
+  --           ignored = "◌",
+  --         },
+  --       },
+  --     },
+  --   },
+  --   diagnostics = {
+  --     enable = true,
+  --     show_on_dirs = true,
+  --     icons = {
+  --       hint = "",
+  --       info = "",
+  --       warning = "",
+  --       error = "",
+  --     },
+  --   },
+  --   view = {
+  --     width = 35,
+  --     side = "left",
+  --     mappings = {
+  --       list = {
+  --         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+  --         { key = "h",                  cb = tree_cb "close_node" },
+  --         { key = "v",                  cb = tree_cb "vsplit" },
+  --       },
+  --     },
+  --   },
+  --   git = {
+  --     enable = true,
+  --     ignore = false,
+  --   },
+  --   actions = {
+  --     open_file = {
+  --       resize_window = true,
+  --     },
+  --   },
+  -- }
 end
 
 return M
