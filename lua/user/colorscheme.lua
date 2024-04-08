bb52add45c64d71621a1d52a79ead98485fdd526
Anh Duc Le <ale@dytechlab.com>
2024-04-08 18:10:29 +0800

    Revert to vscode theme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,23 +1,23 @@
--- local M = {
---     "gmr458/vscode_modern_theme.nvim",
---     lazy = false,
---     priority = 1000,
---     config = function()
---         require("vscode_modern").setup({
---             cursorline = true,
---             transparent_background = true,
---             nvim_tree_darker = true,
---         })
---         vim.cmd.colorscheme("vscode_modern")
---     end,
--- }
-
 local M = {
-  "anhducle98/fleet-theme-nvim",
-  lazy = false,
-  priority = 1000,
-  config = function() vim.cmd("colorscheme fleet") end
+    "gmr458/vscode_modern_theme.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("vscode_modern").setup({
+            cursorline = true,
+            transparent_background = true,
+            nvim_tree_darker = true,
+        })
+        vim.cmd.colorscheme("vscode_modern")
+    end,
 }
+
+-- local M = {
+--   "anhducle98/fleet-theme-nvim",
+--   lazy = false,
+--   priority = 1000,
+--   config = function() vim.cmd("colorscheme fleet") end
+-- }
 
 return M
 
