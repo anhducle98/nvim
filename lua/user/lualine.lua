43dadbf3b0302634e414c69c9b1f5876d9aa8fd2
Anh-Duc Le <anhducle98@gmail.com>
2024-06-23 17:30:24 +0800

    Custom lualine theme

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -18,12 +18,49 @@ function M.config()
     return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
   end
 
+  local custom_theme = function()
+    local colors = {
+      gray     = '#767676',
+      lightred = '#D16969',
+      blue     = '#3B71C2',
+      pink     = '#AE48B6',
+      black    = '#000000',
+      white    = '#E5E5E5',
+      green    = '#56B97F',
+    }
+
+    return {
+      normal = {
+        b = { fg = colors.green, bg = colors.black },
+        a = { fg = colors.black, bg = colors.green, gui = 'bold' },
+        c = { fg = colors.white, bg = colors.black },
+      },
+      visual = {
+        b = { fg = colors.pink, bg = colors.black },
+        a = { fg = colors.black, bg = colors.pink, gui = 'bold' },
+      },
+      inactive = {
+        b = { fg = colors.black, bg = colors.blue },
+        a = { fg = colors.white, bg = colors.gray, gui = 'bold' },
+      },
+      replace = {
+        b = { fg = colors.lightred, bg = colors.black },
+        a = { fg = colors.black, bg = colors.lightred, gui = 'bold' },
+        c = { fg = colors.white, bg = colors.black },
+      },
+      insert = {
+        b = { fg = colors.blue, bg = colors.black },
+        a = { fg = colors.black, bg = colors.blue, gui = 'bold' },
+        c = { fg = colors.white, bg = colors.black },
+      },
+    }
+  end
+
   lualine.setup {
     options = {
       globalstatus = true,
       icons_enabled = true,
-      -- theme = "auto",
-      theme = "ayu",
+      theme = custom_theme,
       component_separators = { left = "", right = "" },
       section_separators = { left = "", right = "" },
       disabled_filetypes = { "alpha", "dashboard", "Nvim-Tree" , "neo-tree" },
