4dbf06409941090d81a072bf077cfde4431807b7
Anh Duc Le <ale@dytechlab.com>
2024-06-24 16:09:11 +0800

    Add clangd path

--- lua/settings/clangd.lua
+++ lua/settings/clangd.lua
@@ -1,7 +1,10 @@
+local bin_path = vim.fn.expand("$HOME/.local/share/nvim/mason/bin/")
+
 return {
   capabilities = {
     offsetEncoding = "utf-16",
   },
+  cmd = { bin_path .. "clangd" },
   args = {
     "-xc++",
     "--background-index",
