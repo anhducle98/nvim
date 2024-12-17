62f8b227caac3999edfc846280fc4d073c737b1b
Anh-Duc Le <anhducle98@gmail.com>
2024-12-17 20:54:30 +0800

    Add smear-cursor

---
+++ lua/user/smear-cursor.lua
@@ -0,0 +1,13 @@
+local M = {
+  "sphamba/smear-cursor.nvim",
+  opts = {                         -- Default  Range
+    stiffness = 0.8,               -- 0.6      [0, 1]
+    trailing_stiffness = 0.5,      -- 0.3      [0, 1]
+    distance_stop_animating = 0.5, -- 0.1      > 0
+    hide_target_hack = false,      -- true     boolean
+    smear_between_neighbor_lines = false,
+  },
+  event = "VeryLazy",
+}
+
+return M
